83f26277c56d60e065d392113f96320ecf855111
Thato khunou <khunou97thato@gmail.com>
2021-02-23 22:00:55 +0200

    Updates

---
+++ precache-manifest.c40e290211f054675748beb8d5ed772c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85cd15e7489760835af0d37b82fdecd",
+    "url": "/ThatoK97/checkout-modal/index.html"
+  },
+  {
+    "revision": "61b993eee53df690e04e",
+    "url": "/ThatoK97/checkout-modal/static/css/main.311a1012.chunk.css"
+  },
+  {
+    "revision": "ebc310d0613250c9472c",
+    "url": "/ThatoK97/checkout-modal/static/js/2.2d289877.chunk.js"
+  },
+  {
+    "revision": "57a73bd50c8c05bdce2b2670e0e1977a",
+    "url": "/ThatoK97/checkout-modal/static/js/2.2d289877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b993eee53df690e04e",
+    "url": "/ThatoK97/checkout-modal/static/js/main.b153ff6e.chunk.js"
+  },
+  {
+    "revision": "ff6d5a4d82fbda27863a",
+    "url": "/ThatoK97/checkout-modal/static/js/runtime-main.60167ae1.js"
+  }
+]);
